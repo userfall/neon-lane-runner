10f26884c7a78198a11c28a0b61c582ad647eed3
userfall <abdoukabirbonfoh1@gmail.com>
2025-08-17 18:41:59 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,13 +1,9 @@
+// Vérifie si mobile
 export function isMobile() {
-  return /Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
+  return /Mobi|Android/i.test(navigator.userAgent);
 }
 
-export function sanitizeKey(s) {
-  if (!s) return "anon";
-  return String(s).replace(/[.#$\[\]\/]/g, "_");
-}
-
-export function formatDate(timestamp) {
-  const d = new Date(timestamp);
-  return d.toLocaleDateString("fr-FR") + " " + d.toLocaleTimeString("fr-FR");
+// Nettoie clé pseudo
+export function sanitizeKey(str) {
+  return str.toLowerCase().replace(/[^a-z0-9]/g, "_");
 }
