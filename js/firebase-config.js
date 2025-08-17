db190b868f837c0885de3f4d22bf5b9c5989277e
userfall <abdoukabirbonfoh1@gmail.com>
2025-08-17 19:33:50 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,7 @@
-// Firebase v9 modular
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCbNweC0aD8f_OGvbvmBVNSKbaMP_A4khI",
   authDomain: "neonlanerunner.firebaseapp.com",
@@ -13,12 +11,8 @@ const firebaseConfig = {
   appId: "1:916907926117:web:43f1d08ed90b864a985c8c"
 };
 
-// Initialisation
 const app = initializeApp(firebaseConfig);
-
-// Services Firebase
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Export
 export { app, auth, db };
